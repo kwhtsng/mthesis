fa3d01bdc66843ce910fbafbcafe1f967f464aa7
kwhtsng <s.kawahito@onomichi-kataoka.jp>
2021-01-15 01:53:03 +0900

    update tex2pdf n README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # mthesis
 * 尾大経情院生用のLaTeXフォーマットです
-    * pLaTeX、upLaTeXでのコンパイルを想定しています
+    * upLaTeX、pLaTeXでのコンパイルを想定しています
+        * デフォルトではuplatex
     * 画像挿入等必要なパッケージは各々自分で選んで適宜書き加えること（dviodfmxとか)
     * 余白・行数等の設定、その他見出しのスタイルは.styファイルで行っています
         * これも必要に応じて書き換えて使ってください
@@ -12,11 +13,11 @@
     * VSCodeとかEmacsとかVimとか好きなやつ
 
 ## Dockerを使ってかんたんにコンパイル
-* bashとdockerさえあれば1コマンドで.texファイルをPDFにできます。
+* bashとdockerさえあれば1コマンドで.texファイルをPDFにできます
 ~~~ bash
 $ bash tex2pdf.sh mthesis.tex
 ~~~
-* upLatexの場合は-uオプションつけるだけ
+* pLatexの場合は-pオプションつけるだけ
 ~~~ bash
-$ bash tex2pdf.sh -u mthesis.tex
+$ bash tex2pdf.sh -p mthesis.tex
 ~~~
--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-uflag="FALSE"
+pflag="FALSE"
 image="docker run --rm -v $(pwd):/workdir paperist/alpine-texlive-ja"
 
 usage(){
-	echo "Usage: $0 [-p] [-u] filename(*.tex)" 1>&2
+	echo "Usage: $0 [-p] filename(*.tex)" 1>&2
 	exit 1
 }
 
-while getopts ad:h OPT
+while getopts :p:h OPT
 do
 	case $OPT in
 		u)  uflag="TRUE"
@@ -22,18 +22,8 @@ done
 
 shift $((OPTIND - 1))
 
-if [ "$uflag" = "TRUE"];
+if [ "$pflag" = "TRUE" ];
 then
-	cat <<- EOS > .latexmkrc
-	#!/usr/bin/env perl
-	\$pdf_mode         = 3;
-	\$latex            = 'uplatex -halt-on-error';
-	\$latex_silent     = 'uplatex -halt-on-error -interaction=batchmode';
-	\$bibtex           = 'upbibtex';
-	\$dvipdf           = 'dvipdfmx %O -o %D %S';
-	\$makeindex        = 'mendex %O -o %D %S';
-	EOS
-else
 	cat <<- EOS > .latexmkrc
 	#!/usr/bin/env perl
 	\$pdf_mode         = 3;
@@ -43,9 +33,18 @@ else
 	\$dvipdf           = 'dvipdfmx %O -o %D %S';
 	\$makeindex        = 'mendex %O -o %D %S';
 	EOS
+else
+	cat <<- EOS > .latexmkrc
+	#!/usr/bin/env perl
+	\$pdf_mode         = 3;
+	\$latex            = 'uplatex -halt-on-error';
+	\$latex_silent     = 'uplatex -halt-on-error -interaction=batchmode';
+	\$bibtex           = 'upbibtex';
+	\$dvipdf           = 'dvipdfmx %O -o %D %S';
+	\$makeindex        = 'mendex %O -o %D %S';
+	EOS
 fi
 
 $image sh -c "latexmk -C $1 && latexmk $1 && latexmk -c $1"
 
 rm -f *.dvi .latexmkrc
-i
