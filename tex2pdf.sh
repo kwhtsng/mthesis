ee74c0f0c465165c37294e676e1189c898611d14
kwhtsng <s.kawahito@onomichi-kataoka.jp>
2021-01-15 01:02:39 +0900

    tex2pdf.sh update

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,9 +1,39 @@
-#!/bin/sh
+#!/bin/bash
 
-file="*.tex"
+uflag="FALSE"
+image="docker run --rm -v $(pwd):/workdir paperist/alpine-texlive-ja"
 
-if [ -f $file ];
+usage(){
+	echo "Usage: $0 [-p] [-u] filename(*.tex)" 1>&2
+	exit 1
+}
+
+while getopts ad:h OPT
+do
+	case $OPT in
+		u)  uflag="TRUE"
+			;;
+		h)  usage_exit
+			;;
+        \?) usage_exit
+			;;
+	esac
+done
+
+shift $((OPTIND - 1))
+
+if [ "$uflag" = "TRUE"];
 then
+	cat <<- EOS > .latexmkrc
+	#!/usr/bin/env perl
+	\$pdf_mode         = 3;
+	\$latex            = 'uplatex -halt-on-error';
+	\$latex_silent     = 'uplatex -halt-on-error -interaction=batchmode';
+	\$bibtex           = 'upbibtex';
+	\$dvipdf           = 'dvipdfmx %O -o %D %S';
+	\$makeindex        = 'mendex %O -o %D %S';
+	EOS
+else
 	cat <<- EOS > .latexmkrc
 	#!/usr/bin/env perl
 	\$pdf_mode         = 3;
@@ -13,10 +43,9 @@ then
 	\$dvipdf           = 'dvipdfmx %O -o %D %S';
 	\$makeindex        = 'mendex %O -o %D %S';
 	EOS
-	docker run --rm -v $(pwd):/workdir paperist/alpine-texlive-ja \
-		sh -c "latexmk -C $1 && latexmk $1 && latexmk -c $1"
-    rm -f *.dvi .latexmkrc
-
-else
-	echo ".texファイルが存在しません。"
 fi
+
+$image sh -c "latexmk -C $1 && latexmk $1 && latexmk -c $1"
+
+rm -f *.dvi .latexmkrc
+i
